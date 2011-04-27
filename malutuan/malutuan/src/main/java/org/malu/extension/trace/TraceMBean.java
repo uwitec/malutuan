235bc0d9c8e6a633c2472190d023917b7adfd42e
malutuan <malutuan@5672265d-eedb-b6b1-4e80-56dd4098b680>
2011-04-27 11:00:17 +0000

    初始导入

---
+++ malutuan/malutuan/src/main/java/org/malu/extension/trace/TraceMBean.java
@@ -0,0 +1,11 @@
+package org.malu.extension.trace;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: 11-4-9
+ * Time: 下午5:41
+ * To change this template use File | Settings | File Templates.
+ */
+public class TraceMBean {
+}
---
+++ malutuan/malutuan/src/main/java/org/malu/modules/orm/GridFilter.java
@@ -0,0 +1,28 @@
+package org.malu.modules.orm;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GridFilter {
+
+	protected String groupOp;
+	protected List<GridRule> rules = new ArrayList<GridRule>();
+
+	public String getGroupOp() {
+		return groupOp;
+	}
+
+	public void setGroupOp(String groupOp) {
+		this.groupOp = groupOp;
+	}
+
+	public List<GridRule> getRules() {
+		return rules;
+	}
+
+	public void setRules(List<GridRule> rules) {
+		this.rules = rules;
+	}
+
+}
